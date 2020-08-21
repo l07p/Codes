0058b15f3780eaf2b80b5ab4dbac94c0aa16fada
l07p <l07p@gmx.de>
2020-08-21 15:27:23 +0000

    first step of creating files

---
+++ cpp20/main.cpp
@@ -0,0 +1,4 @@
+// file: main.cpp
+#include "rect.h"
+#include <iostream>
+int main() { Rect r(3.1, 4.05) std::cout << r.get_area() << std::endl; }
---
+++ cpp20/rect.cpp
@@ -0,0 +1,6 @@
+// file: rect.cpp
+#include "rect.h"
+
+Rect::Rect(double s1, double s2) : side1_(s1), side2_(s2) {}
+
+const double Rect::get_area() const { return side1_ * side2_; }
